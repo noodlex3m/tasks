4b83ce2fafc3527968c0fb14b7f20a820fa09d39
Sergii <noodlex3m@gmail.com>
2025-07-26 23:09:59 +0300

    learn array

--- Частина 3 JavaScript – Мозок Операції (Поглиблений курс)/Модуль 4 Масиви та Об'єкти – Робота з даними/object-array-methods.js
+++ Частина 3 JavaScript – Мозок Операції (Поглиблений курс)/Модуль 4 Масиви та Об'єкти – Робота з даними/object-array-methods.js
@@ -10,3 +10,23 @@
  *  ◦ Відфільтруй повнолітніх: Використовуючи метод .filter(), створи новий масив, в якому будуть тільки студенти, старші за 18 років.
  *  ◦ Виведи інформацію про кожного: Використовуючи метод .forEach(), виведи в консоль рядок для кожного студента у форматі: "Студент [Ім'я], вік [Вік].".
  */
+let students = [
+  { id: 1, name: "Serhii", age: 35, isProgrammer: true },
+  { id: 2, name: "Vova", age: 15, isProgrammer: false },
+  { id: 3, name: "Alina", age: 20, isProgrammer: false },
+  { id: 4, name: "Vetal", age: 45, isProgrammer: true },
+];
+console.log(students);
+const anyProgrammers = students.every((student) => student.isProgrammer);
+console.log(anyProgrammers);
+students.push({ id: 5, name: "Lena", age: 38, isProgrammer: true });
+console.log(students);
+const findStudent = students.find((user) => user.id === 3);
+console.log(findStudent);
+const nameStudents = students.map((userName) => userName.name);
+console.log(nameStudents);
+const adult = students.filter((userAdult) => userAdult.age >= 18);
+console.log(adult);
+students.forEach((userInfo) => {
+  console.log(`Студент ${userInfo.name}, вік ${userInfo.age}`);
+});
